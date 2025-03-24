9e76562b785834263773439f10b2000f981caa4a
Thej <mukundathej@gmail.com>
2025-03-24 12:55:25 +0530

    Update SvlcfBeDeployment.bat

--- support/SvlcfBeDeployment.bat
+++ support/SvlcfBeDeployment.bat
@@ -1,13 +1,13 @@
 cd ..
 timeout /t 2
-docker stop svlcf
-docker rm svlcf
-docker rmi svlcf
+docker stop svlcfbe
+docker rm svlcfbe
+docker rmi svlcfbe
 docker start svlcfmysql
 timeout /t 2
-docker build -t svlcf .
+docker build -t svlcfbe .
 timeout /t 5
-docker run --network svlcf-net --name svlcf -e TZ=Asia/Kolkata -p 9001:9001 -v C:\Data\OneDrive:/home/Data -d svlcf:latest
+docker run --network svlcf-net --name svlcfbe -e TZ=Asia/Kolkata -p 9001:9001 -v C:\Data\OneDrive:/home/Data -d svlcfbe:latest
 timeout /t 25
 cd C:/Data/Code/svlcf-scheduler
 call mvn clean install
